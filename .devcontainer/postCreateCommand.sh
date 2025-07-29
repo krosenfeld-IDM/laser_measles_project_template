57bec243b3bdce18d0444a88d814c20410e6c30e
Katherine Rosenfeld <56368533+krosenfeld-IDM@users.noreply.github.com>
2025-07-29 09:53:45 -0700

    Update postCreateCommand.sh

--- .devcontainer/postCreateCommand.sh
+++ .devcontainer/postCreateCommand.sh
@@ -6,7 +6,7 @@ apt update && apt install -y gh jq
 
 curl -LsSf https://astral.sh/uv/install.sh | sh
 
-curl -qL https://www.npmjs.com/install.sh | bash
+curl -qL https://www.npmjs.com/install.sh | sh
 export NVM_DIR="$HOME/.nvm"
 [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"
 nvm install node
